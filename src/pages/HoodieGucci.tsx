f0d91e8270a83734f4e943fdd6a9de0f493e7cd8
Artem Boichuk <artemboichuk10@gmail.com>
2023-06-28 11:01:27 +0300

    added gucci and prada items

---
+++ src/pages/HoodieGucci.tsx
@@ -0,0 +1,23 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
+
+export function HoodieGucci() {
+  const filteredItems = storeItems.filter(
+    (item) => item.type === "hoodie" && item.brand === "Gucci"
+  );
+
+  return (
+    <>
+      <h1>Gucci Hoodies</h1>
+
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {filteredItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
